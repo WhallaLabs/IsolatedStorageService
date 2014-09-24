e6ad36dca3e7edc6dc8fcaf81c882a3801694a83
razor118 <karol.szmaj@whallalabs.com>
2014-09-24 13:56:34 +0200

    Isolated Storage Service

---
+++ WhallaToolkit.IsolatedStorage/IIsolatedStorageService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhallaToolkit.IsolatedStorage
+{
+    public interface IIsolatedStorageService
+    {
+        /// <summary>
+        /// Saves file into storage at specified name. Serializes payload into json string.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        Task SaveFileAsync(string filename, object payload);
+
+        /// <summary>
+        /// Loads file at specified name. It deserialize json content from storage.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        Task<T> LoadFileAsync<T>(string filename);
+
+        /// <summary>
+        /// Loads raw file at specified name
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        Stream LoadRawFileAsync(string filename);
+
+        /// <summary>
+        /// Saves file into storage at specified name. Serializes payload into json string. 
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="payload"></param>
+        void SaveFile(string filename, object payload);
+
+        /// <summary>
+        /// Loads file and deserializes file content as generic parameter.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        T LoadFile<T>(string filename);
+
+        /// <summary>
+        /// Removes file from storage
+        /// </summary>
+        /// <param name="filename"></param>
+        void RemoveFile(string filename);
+
+        /// <summary>
+        /// Saves the object by specified key on IsolatedStorageSettings. Object is NOT serialized by json converter.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="payload">If you want store custom object class, pass it as json string</param>
+        void SaveSettings(string key, object payload);
+
+        /// <summary>
+        /// Removes settings form key.
+        /// </summary>
+        /// <param name="key"></param>
+        void RemoveSettings(string key);
+
+
+        /// <summary>
+        /// Loads object from settings
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        T LoadSettings<T>(string key);
+        
+    }
+
+
+}
---
+++ WhallaToolkit.IsolatedStorage/IsolatedStorageService.cs
@@ -0,0 +1,211 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace WhallaToolkit.IsolatedStorage
+{
+    public class IsolatedStorageService : IIsolatedStorageService
+    {
+        #region Fields
+        IsolatedStorageSettings settings;
+        #endregion
+
+        #region Interface Implementation
+        public void SaveSettings(string key, object payload)
+        {
+            try
+            {
+                settings[key] = payload;
+            }
+            catch (Exception)
+            {
+                settings.Add(key, payload);
+            }
+            finally
+            {
+                settings.Save();
+            }
+            
+        }
+
+        public void RemoveSettings(string key)
+        {
+            try
+            {
+                settings.Remove(key);
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                settings.Save();
+            }
+        }
+
+        public T LoadSettings<T>(string key)
+        {
+            T result;
+            try
+            {
+                result = (T)settings[key];
+            }
+            catch (Exception)
+            {
+                result = default(T);
+            }
+
+            return result;
+        }
+
+        public async Task SaveFileAsync(string filename, object payload)
+        {
+            using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                using (IsolatedStorageFileStream rawStream = isf.CreateFile(filename))
+                {
+                    var data = await JsonConvert.SerializeObjectAsync(payload);
+                    StreamWriter writer = new StreamWriter(rawStream);
+                    await writer.WriteAsync(data);
+                    await writer.FlushAsync();
+                    writer.Close();
+                }
+            }
+        }
+
+        public Stream LoadRawFileAsync(string filename)
+        {
+            using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (isf.FileExists(filename))
+                {
+                    try
+                    {
+                        using (IsolatedStorageFileStream rawStream = isf.OpenFile(filename, System.IO.FileMode.Open))
+                        {
+                            MemoryStream ms = new MemoryStream();
+
+                            byte[] readBuffer = new byte[4096];
+                            int bytesRead = -1;
+
+                            // Copy the file from the installation folder to the local folder. 
+                            while ((bytesRead = rawStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+                            {
+                                ms.Write(readBuffer, 0, bytesRead);
+                            }
+
+                            ms.Flush();
+                            ms.Seek(0, SeekOrigin.Begin);
+
+                            return ms;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public async Task<T> LoadFileAsync<T>(string filename)
+        {
+            T result = default(T);
+            using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (isf.FileExists(filename))
+                {
+                    try
+                    {
+                        using (IsolatedStorageFileStream rawStream = isf.OpenFile(filename, System.IO.FileMode.Open))
+                        {
+                            StreamReader reader = new StreamReader(rawStream);
+                            var json = await reader.ReadToEndAsync();
+                            result = await JsonConvert.DeserializeObjectAsync<T>(json);
+                            reader.Close();
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public void RemoveFile(string filename)
+        {
+            using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (isf.FileExists(filename))
+                {
+                    try
+                    {
+                        isf.DeleteFile(filename);
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+
+        public void SaveFile(string filename, object payload)
+        {
+            using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                using (IsolatedStorageFileStream rawStream = isf.CreateFile(filename))
+                {
+                    var data = JsonConvert.SerializeObject(payload);
+                    StreamWriter writer = new StreamWriter(rawStream);
+                    writer.Write(data);
+                    writer.Flush();
+                    writer.Close();
+                }
+            }
+        }
+
+        public T LoadFile<T>(string filename)
+        {
+            T result = default(T);
+            using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (isf.FileExists(filename))
+                {
+                    try
+                    {
+                        using (IsolatedStorageFileStream rawStream = isf.OpenFile(filename, System.IO.FileMode.Open))
+                        {
+                            StreamReader reader = new StreamReader(rawStream);
+                            var json = reader.ReadToEnd();
+                            result = JsonConvert.DeserializeObject<T>(json);
+                            reader.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+
+            return result;
+        }
+        #endregion
+
+        #region Constructor
+        public IsolatedStorageService()
+        {
+            settings = IsolatedStorageSettings.ApplicationSettings;
+        }
+        #endregion
+    }
+}
